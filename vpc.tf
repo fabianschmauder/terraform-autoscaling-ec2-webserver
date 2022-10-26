32bde5c5d8f2dde6fdf21fafbf8dce8273ad2f36
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-10-26 14:41:27 +0200

    Add subnets

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,43 @@
 resource "aws_vpc" "application_vpc" {
   cidr_block = var.vpc_cidr_block
 }
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id            = aws_vpc.application_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id            = aws_vpc.application_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.application_vpc.id
+
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.application_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Public route table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
